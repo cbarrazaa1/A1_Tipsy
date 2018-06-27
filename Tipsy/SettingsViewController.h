fa3b2e9a36d97e333bbb8d3a0c71f3952d9483b7
César Francisco Barraza <cbarrazaa1@fb.com>
2018-06-26 17:55:09 -0700

    Required and a bit of optional. TODO Settings

---
+++ Tipsy/SettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.h
+//  Tipsy
+//
+//  Created by César Francisco Barraza on 6/26/18.
+//  Copyright © 2018 Facebook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UIViewController
+
+@end
--- Tipsy/ViewController.m
+++ Tipsy/ViewController.m
@@ -10,13 +10,22 @@
 
 @interface ViewController ()
 
+// control definitions
+@property (weak, nonatomic) IBOutlet UITextField* txtBill;
+@property (weak, nonatomic) IBOutlet UILabel* lblTip;
+@property (weak, nonatomic) IBOutlet UILabel* lblTotal;
+@property (weak, nonatomic) IBOutlet UILabel* lblPercent;
+@property (weak, nonatomic) IBOutlet UIStepper* stpTip;
+@property (weak, nonatomic) IBOutlet UIView* vwTip;
+@property (weak, nonatomic) IBOutlet UIView *vwTotal;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self.txtBill becomeFirstResponder];
 }
 
 
@@ -25,5 +34,59 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)onTap:(id)sender {
+    // hide the keyboard
+    [self.view endEditing:YES];
+}
 
+- (IBAction)onEdit:(id)sender {
+    double bill = [self.txtBill.text doubleValue];
+    double tipPercent = self.stpTip.value / 100;
+    double tip = tipPercent * bill;
+    double total = bill + tip;
+    
+    NSString* text = self.txtBill.text;
+    NSNumberFormatter* formatter = [NSNumberFormatter new];
+    [formatter setNumberStyle:NSNumberFormatterDecimalStyle];
+    NSString* formatted = [formatter text];
+    
+    self.txtBill.text = [NSString stringWithString: formatted];
+    self.lblTip.text = [NSString stringWithFormat:@"$%.2f", tip];
+    self.lblTotal.text = [NSString stringWithFormat:@"$%.2f", total];
+}
+
+- (IBAction)onEditingDidBegin:(id)sender {
+    double val = [self.txtBill.text doubleValue];
+    if(val == 0)
+    {
+        self.txtBill.text = @"";
+    }
+    
+    CGRect frameTip = self.vwTip.frame;
+    CGRect frameTotal = self.vwTotal.frame;
+    frameTip.origin.y = 181;
+    frameTotal.origin.y = 259;
+    
+    [UIView animateWithDuration:0.32 animations:^{
+        self.vwTip.frame = frameTip;
+        self.vwTotal.frame = frameTotal;
+    }];
+}
+
+- (IBAction)onEditingDidEnd:(id)sender {
+    CGRect frameTip = self.vwTip.frame;
+    CGRect frameTotal = self.vwTotal.frame;
+    frameTip.origin.y = 398;
+    frameTotal.origin.y = 475;
+    
+    [UIView animateWithDuration:0.22 animations:^{
+        self.vwTip.frame = frameTip;
+        self.vwTotal.frame = frameTotal;
+    }];
+}
+
+- (IBAction)onValueChanged:(id)sender {
+    int value = self.stpTip.value;
+    self.lblPercent.text = [NSString stringWithFormat:@"%i%%", value];
+}
 @end
